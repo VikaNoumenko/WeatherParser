9533d49470d24e548993dc05c71f5c380af0a7a8
VikaNoumenko <vika.noumenko@gmail.com>
2017-10-17 22:10:01 +0400

    Add JUnit tests

---
+++ src/test/java/vnoumenko/WeatherServiceTest.java
@@ -0,0 +1,47 @@
+package vnoumenko;
+
+import com.github.fedy2.weather.data.Channel;
+import org.junit.Assert;
+import org.junit.Test;
+import vnoumenko.exception.WeatherServiceException;
+
+import static org.junit.Assert.*;
+
+/**
+ * 17.10.2017
+ * WeatherServiceTest
+ *
+ * @author Victoria Noumenko
+ * @version v1.0
+ */
+
+public class WeatherServiceTest {
+
+
+    WeatherService weatherService = new WeatherService();
+
+
+    @Test
+    public void testGetWeather() throws Exception {
+        try {
+            Channel ch = weatherService.getWeather("Moscow");
+            Assert.assertNotNull(ch);
+        } catch (WeatherServiceException e) {
+            Assert.fail("fail");
+        }
+    }
+
+
+    /*
+    @Test
+    public void testGetWeatherNullCity() {
+        try {
+            weatherService.getWeather(null);
+            Assert.fail("fail");
+        } catch (WeatherServiceException e) {
+            Assert.assertTrue(Error.INVALID_CITY == e.getError());
+        }
+    }
+
+*/
+}
